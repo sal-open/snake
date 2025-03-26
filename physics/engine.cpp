5b01a362cf7bf2a1ad22f4552f1570da2408a5f9
Sal-open <salvatorebruzzese05@gmail.com>
2025-03-26 13:56:30 +0100

    Prime righe di codice per l'implementazione del motore fisico del gioco.
    Il codice è in fase sperimentale, non è ancora stato testato, perciò non ci sono
    particolari da segnalare nel commit.

--- physics/engine.cpp
+++ physics/engine.cpp
@@ -4,5 +4,27 @@
 * Course: Programmazione (00819).
 * Author: Salvatore Bruzzese.
 * 
-* Description:
+* Description: Codice sorgente per il motore fisico del gioco.
 */
+
+#include <engine.hpp>
+
+void Screen::screen_size() {
+    getmaxyx(win, screen.y, screen.x);
+}
+
+void Screen::snake_gen() {
+    if (snake_presence == false) {
+        snake_presence = true;
+        snake_pos.x = rand() % screen.x;
+        snake_pos.y = rand() % screen.y;
+    }
+
+    // Per evitare che il serpente tocchi i bordi della finestra corrente.
+
+    if (snake_pos.x == 0) screen.x = screen.x + 10;
+    else if (snake_pos.x == screen.x) screen.x = screen.x - 10;
+
+    if (snake_pos.y == 0) screen.y = screen.y + 20;
+    else if (snake_pos.y == screen.y) screen.y = screen.y - 20;
+}
--- physics/engine.hpp
+++ physics/engine.hpp
@@ -4,10 +4,36 @@
 * Course: Programmazione (00819).
 * Author: Salvatore Bruzzese.
 * 
-* Description:
+* Description: File di libreria per engine.cpp
 */
 
 #ifndef ENGINE_H
 #define ENGINE_H
+
+#include <iostream>
+#include <ncurses.h>
+
+struct pair {
+    int x;
+    int y;
+};
+
+class Screen {
+    private:
+        pair screen;
+        pair snake_pos;
+
+        WINDOW *win;
+
+        bool snake_presence = false;
+
+
+    public:
+        Screen();
+
+        void screen_size();
+
+        void snake_gen();
+};
 
 #endif //ENGINE_H
